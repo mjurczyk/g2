1ff95ae475b24ad353c43f91974ba68e3e82e05e
Maciek Jurczyk <thelairmail@gmail.com>
2018-03-03 01:27:08 +0100

    Adjust unnecessary requests

--- src/global.js
+++ src/global.js
@@ -8,7 +8,7 @@ const Theme = require('./theme/index');
 // const Global = {};
 const Global = {
   version: '____G2_VERSION____',
-  trackable: true,
+  // trackable: true,
   animate: true,
   snapArray: [ 0, 1, 2, 4, 5, 10 ],
   // 指定固定 tick 数的逼近值
